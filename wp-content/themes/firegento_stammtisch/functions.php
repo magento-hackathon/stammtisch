186792bf249c28b7915938591a81b44a4a7c871f
Mathias Elle <dermatz82@gmail.com>
2015-05-09 16:10:39 +0200

    [TASK] added new stammtisch.css files (with less-imports)

--- wp-content/themes/firegento_stammtisch/functions.php
+++ wp-content/themes/firegento_stammtisch/functions.php
@@ -107,9 +107,11 @@ function firegento_stammtisch_scripts() {
 
     wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', array( 'jquery' ), '3.3.4', true );
     wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css', array(), '3.3.4', 'all' );
+    wp_register_style( 'stammtisch-css', get_template_directory_uri() . '/css/stammtisch.css', array(), '1.0.0', 'all' );
 
     wp_enqueue_script( 'bootstrap-js' );
     wp_enqueue_style( 'bootstrap-css' );
+    wp_enqueue_style( 'stammtisch-css' );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
