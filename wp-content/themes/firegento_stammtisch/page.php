a97d1973bb74a0b6ada4eab80f52608aa8c7baa8
Mathias Elle <dermatz82@gmail.com>
2015-05-10 13:56:10 +0200

    [TASK] Updated Page_Classes for Bootstrap Container

--- wp-content/themes/firegento_stammtisch/page.php
+++ wp-content/themes/firegento_stammtisch/page.php
@@ -11,7 +11,7 @@
  */
 
 get_header(); ?>
-
+<div class="container">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -30,6 +30,6 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+</div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
