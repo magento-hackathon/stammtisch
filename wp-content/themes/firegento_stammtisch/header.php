6a744a8ea45827b1d920c4427cade49dafa09c89
Mathias Elle <dermatz82@gmail.com>
2015-05-10 10:56:35 +0200

    [TASK] changed Slider to Fullwidth

--- wp-content/themes/firegento_stammtisch/header.php
+++ wp-content/themes/firegento_stammtisch/header.php
@@ -18,6 +18,61 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+<!-- /.carousel -->
+<div id="Firegento_Stammtisch_Carousel" class="carousel slide">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="http://images.magento-stammtisch-hamburg.de/files/slideshow/hamburg.jpg" style="width:100%" class="img-responsive">
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>Magento Stammtisch in Hamburg</h1>
+                    <p class="sliderdescriotion" >Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                    <p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com">In Hamburg einchecken</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="http://images.magento-stammtisch-hamburg.de/files/slideshow/Location.jpg" class="img-responsive">
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>Magento Stammtisch veröffentlichen</h1>
+                    <p class="sliderdescriotion" >Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                    <p><a class="btn btn-large btn-primary" href="#">Learn more</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="http://images.magento-stammtisch-hamburg.de/files/slideshow/People.jpg" class="img-responsive">
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>All Magento Events</h1>
+                    <p class="sliderdescriotion">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+                    <p><a class="btn btn-large btn-primary" href="#">Browse gallery</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Controls -->
+    <a class="left carousel-control" href="#Firegento_Stammtisch_Carousel" data-slide="prev">
+        <span class="icon-prev"></span>
+    </a>
+    <a class="right carousel-control" href="#Firegento_Stammtisch_Carousel" data-slide="next">
+        <span class="icon-next"></span>
+    </a>
+</div>
+<!-- /.carousel -->
+
+
+
+
 <div id="page" class="hfeed site container">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'firegento_stammtisch' ); ?></a>
 	<header id="masthead" class="site-header" role="banner">
@@ -52,56 +107,6 @@
         <!-- #site-navigation -->
 	</header><!-- #masthead -->
 
-    <!-- /.carousel -->
-        <div id="Firegento_Stammtisch_Carousel" class="carousel slide">
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="item active">
-                    <img src="http://images.magento-stammtisch-hamburg.de/files/slideshow/hamburg.jpg" style="width:100%" class="img-responsive">
-                    <div class="container">
-                        <div class="carousel-caption">
-                            <h1>Magento Stammtisch in Hamburg</h1>
-                            <p class="sliderdescriotion" >Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-                            <p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com">In Hamburg einchecken</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="http://images.magento-stammtisch-hamburg.de/files/slideshow/Location.jpg" class="img-responsive">
-                    <div class="container">
-                        <div class="carousel-caption">
-                            <h1>Magento Stammtisch veröffentlichen</h1>
-                            <p class="sliderdescriotion" >Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-                            <p><a class="btn btn-large btn-primary" href="#">Learn more</a></p>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="http://images.magento-stammtisch-hamburg.de/files/slideshow/People.jpg" class="img-responsive">
-                    <div class="container">
-                        <div class="carousel-caption">
-                            <h1>All Magento Events</h1>
-                            <p class="sliderdescriotion">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-                            <p><a class="btn btn-large btn-primary" href="#">Browse gallery</a></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Controls -->
-            <a class="left carousel-control" href="#Firegento_Stammtisch_Carousel" data-slide="prev">
-                <span class="icon-prev"></span>
-            </a>
-            <a class="right carousel-control" href="#Firegento_Stammtisch_Carousel" data-slide="next">
-                <span class="icon-next"></span>
-            </a>
-        </div>
-    <!-- /.carousel -->
 
 
 
